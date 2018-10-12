394f106a2e1b8c86b562d7b4c5c7e369ca9a10cd
Thiago Colares <tscolares@live.com>
2018-10-12 12:17:05 -0300

    Tratando erros com excecao personalizada

--- JeuDechecsConsole/Tabuleiro/Tabuleiro.cs
+++ JeuDechecsConsole/Tabuleiro/Tabuleiro.cs
@@ -18,10 +18,44 @@
             return Pecas[linha, coluna];
         }
 
-        public void colocarPeca(Peca p, Posicao pos)
+        public Peca Peca(Posicao pos)
         {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
+
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicao Inválida!");
+            }
         }
     }
 }
---
+++ JeuDechecsConsole/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JeuDechecsConsole.tabuleiro
+{
+    public class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base (msg)
+        {
+
+        }
+    }
+}
