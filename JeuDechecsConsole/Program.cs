19726bd35dcfef944002175ec858cee29a6d64bf
Thiago Colares <tscolares@live.com>
2018-10-13 11:51:22 -0300

    Criando a classe PosicaoXadrez

--- JeuDechecsConsole/Program.cs
+++ JeuDechecsConsole/Program.cs
@@ -8,23 +8,11 @@ namespace JeuDechecsConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.WriteLine(pos);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
+            Console.WriteLine(pos.ToPosicao());
 
             Console.ReadLine();
         }
