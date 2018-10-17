efc8cfa19f8a4fad5ceef66d760fc07cb4d37620
Thiago Colares <tscolares@live.com>
2018-10-17 11:53:57 -0300

    Jogada especial promocao

--- JeuDechecsConsole/xadrez/PartidaDeXadrez.cs
+++ JeuDechecsConsole/xadrez/PartidaDeXadrez.cs
@@ -149,6 +149,22 @@ namespace JeuDechecsConsole.xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #Jogada Especial - Promoção
+
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Branca && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -167,8 +183,6 @@ namespace JeuDechecsConsole.xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             // # Jogada Especial - En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
